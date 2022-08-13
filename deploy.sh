8941764740c4daf3d337fcef8e1c4281f5ce8943
calmhand <johnny.goulb@gmail.com>
2022-08-13 01:41:48 -0400

    ghpages

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,4 @@ git add -A
 
 git commit -m 'latest build'
 
-git push -f git@github.com:calmhand/portfolio-site.git master:gh-pages
+git push -f git@github.com:calmhand/portfolio.git master:gh-pages
